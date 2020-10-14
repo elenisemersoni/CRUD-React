c67a9f343676438e942d582094d899d690d569ea
Elenise Mersoni <elenisermersoni@gmail.com>
2020-10-13 22:01:41 -0300

    atualização2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link, Route} from 'react-router-dom';
+import routesConfig from './routesConfig';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <Link className='=px-2' to='/'>Home</Link>
+          <Link className='px-2' to='/user'>User</Link>
+          <Link className='px-2' to='/login'>Login</Link>
+        </nav>
       </header>
+      {routesConfig.map(
+        (value, key) => {
+          return <Route 
+          key={key} 
+          path={value.path} 
+          component={value.component} 
+          exact={value.exact}
+          ></Route>
+        }
+      )}
     </div>
   );
 }
+
 
 export default App;
